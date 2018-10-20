d83d62c41619b674cdfce747f04efd0731557c86
keithchima <chima.3@osu.edu>
2018-10-20 12:58:22 -0400

    Add the empty string test. It actually already passes with no changes

--- StringCalcTest.py
+++ StringCalcTest.py
@@ -6,8 +6,11 @@ class TestStringMethods(unittest.TestCase):
     def setUp(self):
         self.calculator = TDDKata()
 
-    def test_upper(self):
+    def test_zero(self):
         self.assertEqual(self.calculator.string_calc("0"), 0)
+
+    def test_empty_string(self):
+        self.assertEqual(self.calculator.string_calc(""), 0)
 
 if __name__ == '__main__':
     unittest.main()
