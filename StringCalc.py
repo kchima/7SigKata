39bd176d8da8e67cc7d2172862b9181aeeed7eba
keithchima <chima.3@osu.edu>
2018-10-20 14:16:15 -0400

    Fix string_calc to support dynamic delimeter input. Refactored to separate some repeated code into sub-functions

--- StringCalc.py
+++ StringCalc.py
@@ -1,25 +1,38 @@
 class TDDKata:
 
     def string_calc(self, input):
-        intArray = input.split(',')
         total = 0
+        if input == "":
+            return 0
+        if len(input) > 5 and input[0] == '/' and input [1] == '/':
+            delim = input[2]
+            print input[4:]
 
-        if self.array_has_ints(intArray):
-            for integer in intArray:
-                total += int(integer)
+            intArray = self.get_valid_array_for_delim(input[4:], delim)
+            total = self.string_calc_for_delimeter(intArray, delim)
         else:
-            intArray = input.split('\n')
-            if self.array_has_ints(intArray):
-                for integer in intArray:
-                    total += int(integer)
+            intArray = self.get_valid_array_for_delim(input, ',')
+            if intArray:
+                total = self.string_calc_for_delimeter(intArray, ',')
+            else:
+                intArray = self.get_valid_array_for_delim(input, '\n')
+                total = self.string_calc_for_delimeter(intArray, '\n')
         return total
 
-    def array_has_ints(self, array):
+    def string_calc_for_delimeter(self, intArray, delim):
+        total = 0
+        for integer in intArray:
+            total += int(integer)
+        return total
+
+    def get_valid_array_for_delim(self, input, delim):
+        # if input is convertible to a valid int array using our delimeter, return the array. Otherwise, return False
+        intArray = input.split(delim)
         try:
-            int(array[0])
+            int(intArray[0])
         except:
             return False
-        return True
+        return intArray
 
 
     def main(self):
